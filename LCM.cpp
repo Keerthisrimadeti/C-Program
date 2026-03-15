700eb65341de58e6e69834870828fe42fda1bc73
Keerthisrimadeti <153889460+Keerthisrimadeti@users.noreply.github.com>
2026-03-15 15:24:13 +0530

    Add files via upload

---
+++ LCM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=5,result=1;
+
+    for(int i=1;i<=n;i++){
+        result*=i;
+    }
+
+    cout<<"Example program: LCM"<<endl;
+    cout<<"Result value: "<<result<<endl;
+
+    return 0;
+}
